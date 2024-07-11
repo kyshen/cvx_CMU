993181b768ec1b1dd9e8c0f67a3a3eff18e36955
kyshen <kyshen2022@gmail.com>
2024-07-11 12:32:38 +0800

    Finish hw2

--- hw2/least_squares_group_lasso.m
+++ hw2/least_squares_group_lasso.m
@@ -9,16 +9,33 @@ X = [ones(n,1),X];
 lambda = 4;
 t = 0.002;
 steps = 1000;
-
 groups = [3,3,2,1,2,1,1,3];
 
-beta = zeros(1+p,1);
+optimal_value = 84.6952;
 
-values = zeros(steps,1);
+beta = zeros(1+p,1);
+error1 = zeros(steps,1);
 for k = 1:steps
     beta = prox_h(beta - t * grad_g(beta, X, y), t, lambda, groups);
-    values(k) = obj(beta, X, y, lambda, groups);
+    error1(k) = obj(beta, X, y, lambda, groups) - optimal_value;
 end
+sol1 = beta;
+
+beta = zeros(1+p,1);
+error2 = zeros(steps,1);
+B = {beta,beta};
+for k = 1:steps
+    v = B{1} + (k-2)/(k+1)*(B{1}-B{2});
+    beta = prox_h(v - t * grad_g(beta, X, y), t, lambda, groups);
+    error2(k) = obj(beta, X, y, lambda, groups) - optimal_value;
+    B{2} = B{1};
+    B{1} = beta;
+end
+sol2 = beta;
+
+plot(log(error1))
+hold on
+plot(log(error2))
 
 
 function z = prox_h(x, t, lambda, groups)
